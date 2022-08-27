08bd110eff2198d41842fca7812b11e695d49e3c
Niraj Kumar <niraj.kumar@brigosha.com>
2022-08-27 12:35:10 +0530

    adding working code

--- protocolAndDelegate/SecondViewController.swift
+++ protocolAndDelegate/SecondViewController.swift
@@ -7,23 +7,63 @@
 
 import UIKit
 
+protocol DataPassKaro{
+//    func dataPass(name: String, address: String, phone: String)
+    func dataPass(object:[String:String])
+}
+
 class SecondViewController: UIViewController {
+
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtAddress: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+    
+    var delegate: DataPassKaro!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonClicked(_ sender: UIButton) {
+        if(txtName.text == ""){
+            alert(title: "Enter Name", mes: "", delegate: self)
+        }
+        else if(txtAddress.text == ""){
+            alert(title: "Enter Address", mes: "", delegate: self)
+        }
+        else if(txtPhone.text == ""){
+            alert(title: "Enter Phone", mes: "", delegate: self)
+        }
+        else{
+//            delegate.dataPass(name: txtName.text!, address: txtAddress.text!, phone: txtPhone.text!)
+            let dic:[String:String] = ["name": txtName.text!, "address": txtAddress.text!, "phone": txtPhone.text!]
+            delegate.dataPass(object: dic)
+//            let button2Alert: UIAlertView = UIAlertView(title: "Title", message: "message",
+//                                 delegate: self, cancelButtonTitle: "OK")
+//            button2Alert.show()
+            
+//            alert(title: "Saved Successfully", mes: "", delegate: self)
+            
+        }
+//        let alert = UIAlertController(title: "Saved Successfully", message: "Go back and see data", preferredStyle: .alert)
+//            
+//             let ok = UIAlertAction(title: "OK", style: .default, handler: { action in
+//             })
+//             alert.addAction(ok)
+//             DispatchQueue.main.async(execute: {
+//                self.present(alert, animated: true)
+//        })
     }
-    */
-
+    
+    func alert(title: String, mes: String, delegate: AnyObject){
+        let alert = UIAlertView()
+        alert.title = title
+        alert.message = mes
+        alert.addButton(withTitle: "OK")
+        alert.delegate = delegate
+        alert.show()
+    }
+    
 }
--- protocolAndDelegate/ViewController.swift
+++ protocolAndDelegate/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, DataPassKaro {
+    
 
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblAddress: UILabel!
+    @IBOutlet weak var lblPhone: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func goToForm(_ sender: Any) {
+        let secondVC = self.storyboard?.instantiateViewController(withIdentifier: "second") as! SecondViewController
+        secondVC.delegate = self
+        self.navigationController?.pushViewController(secondVC, animated: true)
+    }
+     
+//    func dataPass(name: String, address: String, phone: String) {
+//        lblName.text = name
+//        lblAddress.text = address
+//        lblPhone.text = phone
+//    }
+    
+    func dataPass(object: [String : String]) {
+        lblName.text = object["name"]
+        lblAddress.text = object["address"]
+        lblPhone.text = object["phone"]
+    }
+    
+    
 }
 
